10b1c1593f79a9d1c00f91505ed01a489a4bd76d
Marek <mareksztuczka1992@gmail.com>
2020-08-30 22:29:47 +0200

    Init project

--- exc_GIT_init.py
+++ exc_GIT_init.py
@@ -1,7 +1,2 @@
 Lista_zakupów = {'piekarnia':['chleb','bułki','pączek'],'warzywniak':['marchew','seler','rukola']}
-counter = 0
-print('Lista zakupów')
-for shop, products  in Lista_zakupów.items():
-   print('Idę do %s, kupuję tu następujące rzeczy: %s.' %(shop, ", ".join(products)))
-   counter +=len(products)
-print('W sumię kupuję %d produktów.' % counter)
+
